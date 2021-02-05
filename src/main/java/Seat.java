36a317d90f75f1dd12b24eb9450ccc75d38db50a
Jonatan <jonatanmroczek@gmail.com>
2021-02-05 15:46:26 +0100

    Lesson 3 task 1 - added working tests

--- src/main/java/Seat.java
+++ src/main/java/Seat.java
@@ -3,16 +3,25 @@ import org.junit.jupiter.api.Assertions;
 public class Seat {
    private int seatNumber;
    private int rowNumber;
-   private boolean isTaken;
 
-   Seat (int seatNumber, int rowNumber) {
+
+    protected boolean isTaken;
+
+   Seat (int seatNumber, int rowNumber, boolean isTaken) {
        this.seatNumber = seatNumber;
        this.rowNumber = rowNumber;
+       this.isTaken = isTaken;
    }
+    public void setTaken(boolean taken) {
+        isTaken = taken;
+    }
 
-    void isSeatTaken (boolean IsTaken){
-       this.isTaken = IsTaken;
-   }
+    public boolean isTaken() {
+        return isTaken;
+    }
+
+
+   //boolean isTaken()
 
    public String toString() {
         return seatNumber + " " + rowNumber  + " " + isTaken;
--- src/test/java/SeatTestJava1.java
+++ src/test/java/SeatTestJava1.java
@@ -4,17 +4,35 @@ import org.junit.jupiter.api.Assertions;
 public class SeatTestJava1 {
 
 
+
     @Test
-    void shouldReturnSeatNumberAndRow() {
+    void shouldReturnTrue() {
         //given
         int seatNumber = 10;
         int rowNumber = 3;
-
+        boolean isTaken = true;
+        Seat seat = new Seat(seatNumber, rowNumber, true);
 
         //when
-         Seat = Seat.Seat (1,10);
+        boolean result = seat.isTaken();
+
         //then
-        Assertions.assertEquals(, Seat);
+        Assertions.assertEquals(true,result);
+    }
+    @Test
+    void shouldReturnFalse() {
+        //given
+        int seatNumber = 10;
+        int rowNumber = 3;
+        boolean isTaken = true;
+        Seat seat = new Seat(seatNumber, rowNumber, true);
+        seat.setTaken(false);
+
+        //when
+        boolean result = seat.isTaken();
+
+        //then
+        Assertions.assertEquals(false,result);
     }
 
 }
