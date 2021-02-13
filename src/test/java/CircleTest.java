149b40504eb1ebf0678a8e3250412de2b0649be9
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-02-13 17:24:50 +0100

    ShapeInterface first commit

---
+++ src/test/java/CircleTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CircleTest {
+
+    @Test
+    void shouldReturnCircleArea() {
+        //given
+        double radius = 8;
+        Circle circle = new Circle(radius);
+
+        //when
+        double area = circle.area();
+
+        //then
+        Assertions.assertEquals(200.96, area);
+    }
+
+    @Test
+    void shouldReturnCirclePerimeter () {
+        //given
+        double radius = 8;
+        Circle circle = new Circle(radius);
+
+        //when
+        double perimeter = circle.perimeter();
+
+        //then
+        Assertions.assertEquals(50.24, perimeter);
+    }
+}
