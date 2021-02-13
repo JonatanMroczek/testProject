0136fa6062ec1c6265180c0c8be54abd87bc9b89
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-02-13 21:05:30 +0100

    toString triangle first commit

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    private double width = 20;
+    private double height = 15;
+
+    public Triangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public String toString() {
+        return width + " " + height;
+    }
+}
