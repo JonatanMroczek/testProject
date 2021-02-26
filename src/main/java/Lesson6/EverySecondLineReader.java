13baa620bd57d4c661b12189677d68c5add0e7c6
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-02-26 19:26:50 +0100

    Every second line reader first commit

---
+++ src/main/java/Lesson6/EverySecondLineReader.java
@@ -0,0 +1,31 @@
+package Lesson6;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+    public class EverySecondLineReader {
+        public static void main(String[] args) {
+            try {
+                String pathToFile = "..\\data.txt\\data.txt";
+                Path path = Paths.get(pathToFile);
+                String fileContent = Files.readString(path);
+                BufferedReader in = new BufferedReader(new FileReader(pathToFile));
+                int count = 0;
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if (count % 2 == 0) {
+                        System.out.println(line);
+                    }
+                    ++count;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
