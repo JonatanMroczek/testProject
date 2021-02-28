38d4798223ea1c1a192dab05d8ee8b9c03e02848
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-03-01 00:02:37 +0100

    Divisibility by 3 first commit

---
+++ src/main/java/Lesson6/DivisibilityBy3.java
@@ -0,0 +1,24 @@
+package Lesson6;
+
+public class DivisibilityBy3 {
+    public static void main(String[] args) {
+        validate(new int[]{10, 11, 20, 30});
+    }
+
+    public static void validate(int[] values) {
+        int i;
+        for (i = 0; i < 4; i++) {
+            try {
+                if (values[i] % 3 != 0) {
+                    throw new IllegalArgumentException("Liczba:" + values[i] + " Nie jest podzielna przez 3");
+
+                } else System.out.println("Liczba:" + values[i] + " Jest podzielna przez 3");
+
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
