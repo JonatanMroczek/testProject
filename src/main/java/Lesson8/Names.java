f6057555ebe7d9399aca69443c58826bef4944fd
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-03-13 17:51:16 +0100

    Names list first commit

---
+++ src/main/java/Lesson8/Names.java
@@ -0,0 +1,9 @@
+package Lesson8;
+
+import java.util.Iterator;
+
+public class Names {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Lesson8/NamesList.java
@@ -0,0 +1,41 @@
+package Lesson8;
+
+import java.util.*;
+
+public class NamesList {
+    public static void main(String[] args) {
+
+
+        List<String> names = Arrays.asList("Anna", "Katarzyna", "Tymon", "Tadeusz", "Anna", "Karolina", "Dorota", "Piotr");
+
+
+        ListIterator<String> listIterator = names.listIterator();
+        System.out.println("List in normal order:");
+
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+
+        System.out.println("\n" + "List in reverse order: ");
+
+        while (listIterator.hasPrevious()) {
+            System.out.println(listIterator.previous());
+        }
+
+        System.out.println("\n" + "Anna changed to Joanna:");
+
+        names.set(0, "Joanna");
+        while (listIterator.hasNext()) {
+            listIterator.next();
+        }
+        while (listIterator.hasPrevious()) {
+            System.out.println(listIterator.previous());
+        }
+
+    }
+
+}
+
+
+
+
