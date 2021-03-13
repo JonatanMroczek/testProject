e768e090e1918febb1b5e7770bce90ccb77e74f7
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-03-13 20:07:30 +0100

    Names list removed duplicates

--- src/main/java/Lesson8/Names.java
+++
@@ -1,9 +0,0 @@
-package Lesson8;
-
-import java.util.Iterator;
-
-public class Names {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/Lesson8/NamesList.java
+++ src/main/java/Lesson8/NamesList.java
@@ -6,24 +6,37 @@ public class NamesList {
     public static void main(String[] args) {
 
 
-        List<String> names = Arrays.asList("Anna", "Katarzyna", "Tymon", "Tadeusz", "Anna", "Karolina", "Dorota", "Piotr");
+        List<String> names = new ArrayList<>(Arrays.asList("Anna", "Katarzyna", "Tymon", "Tadeusz", "Anna", "Karolina", "Dorota", "Piotr"));
+        List<String> uniqueNamesList = new ArrayList<>();
 
+        Set<String> uniqueNames = new LinkedHashSet<>();
+        uniqueNames.addAll(names);
 
-        ListIterator<String> listIterator = names.listIterator();
+        uniqueNamesList.addAll(uniqueNames);
+
+        ListIterator<String> uniqueNamesListIterator = uniqueNamesList.listIterator();
         System.out.println("List in normal order:");
-        while (listIterator.hasNext()) {
-            System.out.println(listIterator.next());
+        while (uniqueNamesListIterator.hasNext()) {
+            System.out.println(uniqueNamesListIterator.next());
         }
 
+        ListIterator<String> listIterator = names.listIterator();
         System.out.println("\n" + "List in reverse order: ");
+        while (listIterator.hasNext()) {
+            listIterator.next();
+        }
         while (listIterator.hasPrevious()) {
             System.out.println(listIterator.previous());
         }
 
         System.out.println("\n" + "Anna changed to Joanna:");
-        names.set(0, "Joanna");
         while (listIterator.hasNext()) {
-            listIterator.next();
+            String next = listIterator.next();
+            if (next.equals("Anna")) {
+                //Replace element
+                listIterator.set("Joanna");
+            }
+
         }
         while (listIterator.hasPrevious()) {
             System.out.println(listIterator.previous());
