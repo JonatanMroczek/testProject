2b81f4dd5ccd175fca63609a8fbf2d7b52dc0d1a
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-03-13 17:53:48 +0100

    Names list deleted empty space

--- src/main/java/Lesson8/NamesList.java
+++ src/main/java/Lesson8/NamesList.java
@@ -11,19 +11,16 @@ public class NamesList {
 
         ListIterator<String> listIterator = names.listIterator();
         System.out.println("List in normal order:");
-
         while (listIterator.hasNext()) {
             System.out.println(listIterator.next());
         }
 
         System.out.println("\n" + "List in reverse order: ");
-
         while (listIterator.hasPrevious()) {
             System.out.println(listIterator.previous());
         }
 
         System.out.println("\n" + "Anna changed to Joanna:");
-
         names.set(0, "Joanna");
         while (listIterator.hasNext()) {
             listIterator.next();
