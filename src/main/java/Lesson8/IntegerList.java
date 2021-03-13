46626048aa471e58e5493f3bad2d68f92ea65044
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-03-13 23:00:18 +0100

    Own list changed list to array

--- src/main/java/Lesson8/IntegerList.java
+++ src/main/java/Lesson8/IntegerList.java
@@ -1,43 +1,54 @@
 package Lesson8;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 
 public class IntegerList implements OwnList {
 
-   static private List<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
+    public static Integer[] arrayAsList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 
     @Override
     public int size() {
-        return list.size();
+        return arrayAsList.length;
     }
 
     @Override
     public boolean isEmpty() {
-        return list.isEmpty();
+        if (arrayAsList.length == 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void add(Integer element) {
-        list.add(element);
+        arrayAsList = ArrayUtils.add(arrayAsList, element);
     }
+
 
     @Override
     public Integer get(int i) {
-        return list.get(i);
+        return arrayAsList[i];
     }
 
     @Override
-    public void add(int index, Integer element) throws IndexOutOfBoundsException {
-        list.add(index, element);
+    public void add(int index, Integer element) {
+        if (index < 0 || index > arrayAsList.length) {
+            throw new IllegalArgumentException("List does not contain entered index");
+        }
+        arrayAsList = ArrayUtils.add(arrayAsList, index, element);
     }
 
     @Override
     public Integer remove(int index) {
-        return list.remove(index);
+        if (index < 0 || index > arrayAsList.length) {
+            throw new IllegalArgumentException("List does not contain entered index");
+        }
+        arrayAsList = ArrayUtils.remove(arrayAsList, index);
 
+        return null;
     }
 }
